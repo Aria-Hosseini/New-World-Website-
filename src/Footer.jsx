cd522f9854a103d89faf37037df486e96c60c928
vihan <ahsyny356@gmail.com>
2025-09-07 23:48:17 +0330

    liarasetup

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="flex items-center justify-center p-5 bg-neutral-950">
       <div className="flex flex-col items-center justify-center w-300 bg-[#161616]/50 shadow-xl p-6 space-y-4 backdrop-blur-md rounded-2xl">
         <h3 className="text-amber-50 text-lg font-medium">
-          Made with <span className="text-red-500">❤</span> by Vihan
+          Made with <span className="text-red-500">❤</span> by the-Aria
         </h3>
         <div className="flex space-x-6">
           <a
